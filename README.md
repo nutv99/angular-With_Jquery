b1f17d5fd4c57cfd2e36162a09bbc7a42898f445
nutv99 <nutv99@gmail.com>
2022-10-27 17:57:56 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-With_Jquery
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-r9zjnh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, VERSION ,OnInit} from '@angular/core';
+import $ from 'jquery';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+  isJqueryWorking: string
+
+  ngOnInit()
+  {
+    $(document).ready(() => {
+        this.isJqueryWorking = 'Jquery is working 9999!!!'; 
+        $("#testdiv").html('sssss')
+    });
+
+  }
+
+  addClass() {
+    $("#testdiv").html('99999')
+  }
+}
