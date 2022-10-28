169300f2602626055a669db6374e03d96154fc1f
nutv99 <nutv99@gmail.com>
2022-10-28 14:28:59 +0700

    1 file modified, 3 added, 4 deleted

--- src/app/pages/department/department.component.ts
+++ src/app/pages/department/department.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+// import {Tabledata1Component} from 'src/app/_shared/components/tabledata1/tabledata1.compoment' ;
+//import { Tabledata1Component } from '../../shared/components/tabledata1/tabledata1.compoment' ; 
+
+import { CategoryComponent} from '../category/category.component' ;
+
 
 @Component({
   selector: 'app-department',
