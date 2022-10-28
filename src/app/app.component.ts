fc15e43265ffce2044bd2510f3e1c33722396cfc
nutv99 <nutv99@gmail.com>
2022-10-28 07:22:38 +0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,36 @@
-import { Component, VERSION ,OnInit} from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
 import $ from 'jquery';
-
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
-  isJqueryWorking: string
+  isJqueryWorking: string;
 
-  ngOnInit()
-  {
+  ngOnInit() {
     $(document).ready(() => {
-        this.isJqueryWorking = 'Jquery is working 9999!!!'; 
-        $("#testdiv").html('sssss')
+      this.isJqueryWorking = 'Jquery is working 9999!!!';
+      // $('#testdiv').html('sssss');
     });
-
   }
 
   addClass() {
-    $("#testdiv").html('99999')
-    $("#testdiv").addClass('myBorder')
+    //$('#testdiv').html('99999');
+    // if (!$("#testdiv").hasClass('myBorder')) {
+    //   $("#testdiv").addClass('myBorder')
+    // } else {
+    //   $("#testdiv").removeClass('myBorder').addClass('myBorder2')
+    // }
+    $('#testdiv').toggleClass('collapsed');
 
+    // document.querySelector('button').addEventListener('click', () => { document.querySelector('.collapsible').classList.toggle('collapsed');});
+  }
+
+  HideMenu(id: string) {
+    $('#' + id).toggleClass('scale-up-ver-top');
+    // document.querySelector('.ulmenu').toggleClass('collapsedHeight');
   }
 }
