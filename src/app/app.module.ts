e54ff3edeb10acc383661ab691f6af4fd4898b27
nutv99 <nutv99@gmail.com>
2022-10-30 08:53:36 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser'; 
-import { HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule, Routes } from '@angular/router';
@@ -9,30 +9,34 @@ import { AppComponent } from './app.component';
 import { Tabledata1Component } from './shared/components/tabledata1/tabledata1.component';
 import { HelloComponent } from './hello.component';
 import { DepartmentComponent } from './pages/department/department.component';
-import { CategoryComponent } from './pages/category/category.component'; 
+import { CategoryComponent } from './pages/category/category.component';
 import { Tabledata2Component } from './shared/components/tabledata2/tabledata2.component';
-
 
 const appRoutes: Routes = [
   { path: 'department', component: DepartmentComponent },
   { path: 'category', component: CategoryComponent },
-  
-  { path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  
+
+  { path: '', redirectTo: '/', pathMatch: 'full' },
 ];
 
-
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,FontAwesomeModule,HttpClientModule,
+  imports: [
+    BrowserModule,
+    FormsModule,
+    FontAwesomeModule,
+    HttpClientModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: false } // <-- debugging purposes only set true
-    )
-   ],
-  declarations: [ AppComponent,Tabledata1Component, HelloComponent,DepartmentComponent,Tabledata2Component ],
-  bootstrap:    [ AppComponent ]
+    ),
+  ],
+  declarations: [
+    AppComponent,
+    Tabledata1Component,
+    HelloComponent,
+    DepartmentComponent,
+    Tabledata2Component,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
