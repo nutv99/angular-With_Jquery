fc5ad42efcc5b7e36f2955c2dfa2a35a9ed5b121
nutv99 <nutv99@gmail.com>
2022-10-31 13:58:45 +0700

    3 files modified

--- src/app/shared/components/searchselect/searchselect.component.ts
+++ src/app/shared/components/searchselect/searchselect.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-searchselect',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./searchselect.component.css']
 })
 export class SearchselectComponent implements OnInit {
-
+  @ViewChild("myNameElem") myNameElem: ElementRef; 
   constructor() { }
 
   ngOnInit() {
