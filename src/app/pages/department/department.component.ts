10bdf4a698e6027faa9d324fdc3456d13fa3a773
nutv99 <nutv99@gmail.com>
2022-10-31 08:10:09 +0700

    2 files modified

--- src/app/pages/department/department.component.ts
+++ src/app/pages/department/department.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Tabledata2Component } from '../../shared/components/tabledata2/tabledata2.component';
 
+declare var window: any;
 export interface modelTable {
   apiTable: string;
   headerTable: string[];
@@ -12,12 +13,24 @@ export interface modelTable {
   styleUrls: ['./department.component.css'],
 })
 export class DepartmentComponent implements OnInit {
-  
+  formModal: any;
   varmodelTable: modelTable = {
     apiTable: 'department',
     headerTable: ['รหัสแผนก', 'ชื่อแผนก', 'รหัส-2', 'รูป'],
   };
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('myModal')
+    );
+  }
+
+  openFormModal() {
+    this.formModal.show();
+  }
+  saveSomeThing() {
+    // confirm or save something
+    this.formModal.hide();
+  }
 }
