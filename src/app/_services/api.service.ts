75830ac761b886f55b4f96b23667d90769590e99
nutv99 <nutv99@gmail.com>
2022-11-08 12:47:01 +0700

    1 file modified, 1 added

---
+++ src/app/_services/api.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from '../environment';
+import { Emp } from '../_models/emp';
+
+const baseUrl = environment.apiUrl + '/employee';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class APIService {
+  constructor(private http: HttpClient) {}
+
+  // httpOptions = {
+  //     headers: new HttpHeaders({
+  //         'Content-Type': 'application/json',
+  //         "Access-Control-Allow-Origin": "*"
+  //     }), responseType: 'text' as 'json'
+  // };
+
+  httpOptions = {};
+
+  getAll() {
+    return this.http.get<any>(baseUrl);
+  }
+
+  getById(id: number) {
+    return this.http.get<any>(`${baseUrl}/${id}`);
+  }
+  getByPageNo(id: number) {
+    return this.http.get<any>(`${baseUrl}/${id}`);
+  }
+
+  getByParentID(id: number) {
+    return this.http.get<any>(`${baseUrl}/${id}`);
+  }
+
+  getByChild(id: number) {
+    return this.http.get<any>(`${baseUrl}/${id}`);
+  }
+
+  getByChildren(id: number) {
+    return this.http.get<any>(`${baseUrl}/${id}`);
+  }
+
+  create(params: any) {
+    return this.http.post(baseUrl, params, this.httpOptions);
+  }
+
+  update(params: any) {
+    return this.http.put(`${baseUrl}`, params, this.httpOptions);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${baseUrl}/${id}`, this.httpOptions);
+  }
+}
