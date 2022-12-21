afd4ec54fcb5a2fee5159d64e0ef629fc457c801
nutv99 <nutv99@gmail.com>
2022-12-21 09:18:23 +0700

    2 files modified

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,13 +1,14 @@
-// export const environment = {
-//   production: false,
-//   apiHost: 'https://lovetoshopmall.com/swagger/marlinshopWork2/',
-//   apiUrl: 'https://lovetoshopmall.com/swagger/marlinshopWork2',
-// };
 export const environment = {
   production: false,
-  apiHost: 'https://lovetoshopmall.com/apiservice/tshop/',
-  apiUrl: 'https://lovetoshopmall.com/apiservice/tshop/',
+  apiHost: 'https://lovetoshopmall.com/swagger/marlinshopWork2/',
+  apiUrl: 'https://lovetoshopmall.com/swagger/marlinshopWork2',
 };
+
+// export const environment = {
+//   production: false,
+//   apiHost: 'https://lovetoshopmall.com/apiservice/tshop/',
+//   apiUrl: 'https://lovetoshopmall.com/apiservice/tshop/',
+// };
 
 export const apiPath = {
   production: false,
